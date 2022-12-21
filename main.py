cc66e1653d3f03b2912fe34c4bbeb224a4a26276
arbelna <nadav.arbel05@gmail.com>
2022-12-21 13:53:04 +0200

    Update dataframe

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import experiment as ex
 from eeg import Eeg
 import numpy as np
-# eeg = Eeg()
-# exp = ex.Experiment(eeg)
-# eeg.stream_on()
-# eeg.clear_board()
-# exp.run_experiment(eeg)
-# data = eeg.get_stream_data()
-# eeg.stream_off()
-# with open('raw_data.npy', 'wb') as f:
-#     np.save(f, data, allow_pickle=True)
-
-exp = ex.Experiment("eeg")
-exp.run_experiment("eeg")
+eeg = Eeg()
+exp = ex.Experiment(eeg)
+eeg.stream_on()
+eeg.clear_board()
+exp.run_experiment(eeg)
+data = eeg.get_stream_data()
+eeg.stream_off()
+with open('raw_data.npy', 'wb') as f:
+    np.save(f, data, allow_pickle=True)
 
 
-# df = exp.results
-exp.results.to_csv('experiment_results.csv', index=False)
+df = exp.results
+df.to_csv('experiment_results.csv', index=False)
 print(exp)
